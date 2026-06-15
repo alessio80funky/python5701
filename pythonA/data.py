af7f5873a4c951c8030d2cd6ba36707d39e6222f
alessio80funky <alessio80funky@gmail.com>
2026-06-15 12:07:15 +0900

    6/15 12:07

---
+++ pythonA/data.py
@@ -0,0 +1,21 @@
+#データタ型
+
+#ベース型
+
+name = "山田" #str　ストリング型
+age = 19 #int 整数型
+height = 170.5 #float  少数型
+is_student = True #bool #判断型
+
+#参照型
+
+#list
+
+#tuple
+
+print(type(name)) #<class 'str'>
+print(type(age)) #<class 'int'>
+print(type(height))#<class 'float'>
+print(type(is_student))#<class 'bool'>
+
+
--- pythonA/function.py
+++ pythonA/function.py
@@ -3,3 +3,16 @@ def say_hello():
     print("hello i am python teacher")
 
 say_hello()
+
+
+def add(a,b):
+    return a + b
+
+result = add(10, 5)
+
+print(result)
+
+def greet(name):
+    return f"{name}です"
+
+print(greet("田中"))
