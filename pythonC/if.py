e56587fa564520156d842705e171628221cf66af
alessio80funky <alessio80funky@gmail.com>
2026-06-17 11:49:29 +0900

    6/17 11:49

---
+++ pythonC/if.py
@@ -0,0 +1,19 @@
+#if
+
+score = 71
+
+if score >= 80:
+    print("合格です")
+elif score >= 70:
+    print("合格の最低限のライン")
+else:
+    print("不合格")
+
+
+
+value = 301
+
+if value >= 100 and value < 300:
+    print("100円以上300円以下")
+else:
+    print("100円未満300円以上")
---
+++ pythonC/logic.py
@@ -0,0 +1,11 @@
+#比較演算子
+
+a = True
+b = False
+
+print(a and b) #どちらも
+
+print(a or b) #どれか
+
+print(not b) #h否定演算子
+
